e307140ae86e68954162d4fcd154f7df2901e575
evpozz <32553718+evpozz@users.noreply.github.com>
2018-02-01 14:39:42 -0500

    week 3 class

--- p5/gettingstarted/sketch.js
+++ p5/gettingstarted/sketch.js
@@ -1,15 +1,41 @@
-var centerX = 200;
-var centerY = 200;
+var backgroundColor = "white";
+
+var button;
+var button2;
+
+var noseColor = "black";
+
+var sel;
+var eyeColor = "black";
 
 function setup() {
   // put setup code here
   createCanvas(400,400);
   background(255);
+  button = createButton("Click Me");
+  button.mousePressed(changeBG);
+  button2 = createButton("Red");
+  button.mousePressed(function(){
+
+    // your code goes here
+    noseColor = "red";
+  });
+
+  sel = createSelect();
+  sel.option("blue");
+  sel.option("green");
+  sel.option("brown");
+
+  sel.changed(function(){
+    eyeColor = sel.value();
+
+  });
+
 }
 
 function draw() {
   // put drawing code here
-  background(255);
+  background(backgroundColor);
   centerX = mouseX;
   centerY = mouseY;
 
@@ -17,9 +43,17 @@ function draw() {
   strokeWeight(1);
   fill("white");
   rect(centerX -150,centerY -100,100,100);
-  ellipse(centerX -100,centerY -50,50,50);
+  // eye1
+  var xOffset = map(mouseX, 0, width, -20,20);
+  console.log("xOffset: " + xOffset + " yOffset: " + yOffset);
+  var yOffset = map(mouseY, 0, height, -20,20);
+  fill(eyeColor);
+  ellipse(centerX -100 + xOffset,centerY -50 + yOffset,50,50);
+  fill("white");
   rect(centerX +25,centerY -100,100,100);
-  ellipse(centerX + 75,centerY -50,50,50);
+  // eye2
+  fill(eyeColor);
+  ellipse(centerX + 75 + xOffset,centerY -50 + yOffset,50,50);
 
   stroke(125);
   strokeWeight(5);
@@ -29,6 +63,22 @@ function draw() {
   line(centerX -50,centerY +50,centerX +50,centerY +50);
 
   noStroke();
-  fill(0,0,255);
+  fill(noseColor);
+  // nose
   triangle(centerX,centerY,centerX +25,centerY +25,centerX -25,centerY +25);
+
+
+}
+
+
+function mousePressed(){
+  backgroundColor = "blue";
+}
+
+function mouseReleased(){
+  backgroundColor = "red";
+}
+
+function changeBG(){
+  noseColor = "green";
 }
