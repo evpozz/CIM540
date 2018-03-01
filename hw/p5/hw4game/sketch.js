a97c78af7eb6e3887f86a80f63e677f353bbace4
evpozz <32553718+evpozz@users.noreply.github.com>
2018-03-01 14:49:57 -0500

    Class notes

--- hw/p5/hw4game/sketch.js
+++ hw/p5/hw4game/sketch.js
@@ -1,5 +1,5 @@
 
-var targetPosX = [0,200,400,600];
+var targetPosX = [0,200,400,600,800,1000];
 
 var targetPosY = [0,200];
 
@@ -19,14 +19,16 @@ function preload(){
   pics[0] = loadImage("assets/fall.png");
   pics[1] = loadImage("assets/summer.png");
   pics[2] = loadImage("assets/winter.png");
-
+  pics[3] = loadImage("assets/basketball.png");
+  pics[4] = loadImage("assets/Football.png");
+  pics[5] = loadImage("assets/soccer.png");
 
 }
 
 function setup(){
-  createCanvas(600,400);
+  createCanvas(1200,400);
   console.log("First position in targetPosX: " + targetPosX[0]);
-  console.log("3rd position in targetPosX: " + targetPosX[2]);
+  console.log("6th position in targetPosX: " + targetPosX[5]);
   startGame = createButton("Click to Start!");
   startGame.position(width/2, 300);
   startGame.mousePressed(function(){
@@ -43,6 +45,9 @@ function draw(){
   rect(targetPosX[0], targetPosY[0], imageSize, imageSize);
   rect(targetPosX[1], targetPosY[0], imageSize, imageSize);
   rect(targetPosX[2], targetPosY[0], imageSize, imageSize);
+  rect(targetPosX[3], targetPosY[0], imageSize, imageSize);
+  rect(targetPosX[4], targetPosY[0], imageSize, imageSize);
+  rect(targetPosX[5], targetPosY[0], imageSize, imageSize);
 
   if(mouseX >= targetPosX[0] && mouseX < targetPosX[1] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
     console.log("Box 1");
@@ -60,6 +65,24 @@ function draw(){
       console.log("Box 3");
       image(pics[2], targetPosX[2], targetPosY[0])
       currentBox = 3;
+  }
+
+  if(mouseX >= targetPosX[3] && mouseX < targetPosX[4] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
+      console.log("Box 4");
+      image(pics[3], targetPosX[3], targetPosY[0])
+      currentBox = 4;
+  }
+
+  if(mouseX >= targetPosX[4] && mouseX < targetPosX[5] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
+      console.log("Box 5");
+      image(pics[4], targetPosX[4], targetPosY[0])
+      currentBox = 5;
+  }
+
+  if(mouseX >= targetPosX[5] && mouseX < targetPosX[6] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
+      console.log("Box 6");
+      image(pics[5], targetPosX[5], targetPosY[0])
+      currentBox = 6;
   }
 }
   textSize(32);
